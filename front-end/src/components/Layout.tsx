d2b253930090109c26f4c0702661436a4ea15e42
AslamJM <jma8744@gmail.com>
2023-09-07 06:57:00 +0530

    before deploy

--- front-end/src/components/Layout.tsx
+++ front-end/src/components/Layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Drawer from "./Drawer";
 import { Button } from "./ui/button";
 import { AlignJustify } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [open, setIsOpen] = React.useState(false);
@@ -11,6 +12,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <Button className="m-2" onClick={() => setIsOpen(true)}>
           <AlignJustify />
         </Button>
+        <Link to="/dashboard" className="text-white">
+          Dashboard
+        </Link>
       </div>
       <Drawer isOpen={open} setIsOpen={setIsOpen} />
       {children}
