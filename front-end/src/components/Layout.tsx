74128a9d8d57b598e4d498a492d23f23ef80f18d
AslamJM <jma8744@gmail.com>
2023-09-12 12:29:57 +0530

    protected routes and login functionalities

--- front-end/src/components/Layout.tsx
+++ front-end/src/components/Layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Drawer from "./Drawer";
 import { Button } from "./ui/button";
 import { AlignJustify } from "lucide-react";
-import { Link } from "react-router-dom";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [open, setIsOpen] = React.useState(false);
@@ -12,7 +11,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <Button className="m-2" onClick={() => setIsOpen(true)}>
           <AlignJustify />
         </Button>
-       
       </div>
       <Drawer isOpen={open} setIsOpen={setIsOpen} />
       {children}
